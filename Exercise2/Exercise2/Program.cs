ad4d388422cd547de53bd894c213023527b098ca
Anurag Shrivastava <anurags@usf.edu>
2020-01-21 21:00:44 -0500

    Program

---
+++ Exercise2/Exercise2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            int height;
+            //max Heart rate is equal to 220-age
+            age = 27;
+            height = 158;
+            int heartrate;
+            heartrate = 220 - age;
+            Console.WriteLine("I am " + age + "years old , my max heart rate" +  heartrate);
+
+            
+
+        
+            Console.ReadLine();
+            
+
+        }
+    }
+}
